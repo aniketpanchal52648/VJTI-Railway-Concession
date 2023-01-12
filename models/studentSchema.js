9bdf0e53c04a1a505c9a55e7b0659988556a9bc2
aniket52648 <aniketpanchal52648@gmail.com>
2023-01-12 16:19:16 +0530

    feat: auth

--- models/studentSchema.js
+++ models/studentSchema.js
@@ -1,14 +1,31 @@
-const mongoose =require('mongoose');
-const Schema=mongoose.Schema
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const passportLocalM=require('passport-local-mongoose');
 const studentSchema= new Schema({
-    username:String,
-    password:String,
-    first_name:String,
-    last_name:String,
-    email:String
+    email:{
+        type:String,
+        required:true,
+        unique:true
+        
+    },
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    first_name:{
+        type:String,
+        require:true
+    },
+    last_name:{
+        type:String,
+        require:true
+    }
 
 
 
 });
+studentSchema.plugin(passportLocalM);
+// module.exports=mongoose.model('User',userScheama);
 module.exports=mongoose.model('Student',studentSchema);
 

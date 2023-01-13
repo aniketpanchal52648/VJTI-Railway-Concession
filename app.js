6a94dc3b2d1a5802b0905c4c05358e0ea407ae05
aniket52648 <aniketpanchal52648@gmail.com>
2023-01-14 01:44:57 +0530

    working on kaks side

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); 
 const express=require('express');
 const ejsMate=require('ejs-mate');
 const app=express();
@@ -19,7 +20,7 @@ const upload=multer({storage});
 
 
 
-require('dotenv').config(); //dotenv package
+//dotenv package
 
 app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
@@ -132,16 +133,49 @@ app.get('/institute_view/aprroved', async(req,res)=>{
     console.log(Requests);
     res.render('accepted',{Requests});
 })
-app.get('/institute_view/:id',(req,res)=>{
-    const id=req.params.id
-    res.send(id);
+app.get('/institute_view/:id',async(req,res)=>{
+    const id=req.params.id;
+    const reqs=await Reuqest.findById(id).populate('student');
+    console.log(reqs);
+
+
+    res.render('view',{reqs});
     // res.render('institute_view');
 })
 app.get('/accepted',(req,res)=>{
     res.render('accepted');
 })
 
+app.get('/institue_view/accepted/:id', async(req,res)=>{
+    const request=await Request.findById(req.params.id);
+    request.request_status=true;
+    await request.save();;
+    res.redirect('/institue_view');
 
+})
+app.get('/institue_view/reverted/:id',(req,res)=>{
+    
+})
+app.post('/institue_view/reverted/:id',async(req,res)=>{
+    const req_id=req.params.id
+    const request=await Request.findById(req_id).populate('student');
+    const id=request.student._id;
+    await Reuqest.findByIdAndDelete(req_id);
+    const student=await Student.findById(id);
+    const mailOptions = {
+        from: "vjtirailwayconcession@gmail.com",
+        to: student.email,
+        subject: "Request Reverted",
+        // text: "Testing mail sending using NodeJS"
+        html: `<p>Dear ${student.first_name} ${student.last_name}, your application for the concession has been reverted back due to the provided <strong> ${req.body.reason} </strong>.</p> <p> You may now check the status of the application and make the required changes and re-apply for the application. </p>` 
+    };
+
+    sendMail(mailOptions);
+    res.redirect('/institue_view');
+
+
+
+})
 app.post('/application/',passport.authenticate('local',{failureFlash:true,failureRedirect:'/'}), async(req,res)=>{
 
 
@@ -182,7 +216,7 @@ app.get('/application/uploaddoc/:id', async(req,res)=>{
     // const caste=st.caste
     res.render('documents',{st});
 })
-app.post('/application/uploaddoc/:id',upload.array('image1','image2','image3'),async(req,res)=>{
+app.post('/application/uploaddoc/:id',upload.array('image',3),async(req,res)=>{
     const student=await Student.findById(req.params._id);
     console.log(req.files);
     res.send('done');

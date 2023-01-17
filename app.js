838239252c7f6f9085db5c6e14d3919c646059b6
aniket52648 <aniketpanchal52648@gmail.com>
2023-01-17 20:23:45 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if(process.env.NODE_ENV !=='production'){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const ejsMate = require('ejs-mate');
 const app = express();
